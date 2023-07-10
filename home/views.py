e0d023856f423b1a332c87ec981431624c182ce9
robinson <robinpaulista85@gmail.com>
2023-07-10 15:44:36 -0300

    Aninhando URLS com path, include e urls.py dos apps do Django

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 def home(request):
     print('home')
-    return HttpResponse('Hello from home!')
+    return HttpResponse('Home do app 1')
