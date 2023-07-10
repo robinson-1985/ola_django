8b24146aa58122eb8c0a9559713e2f6dd4f24681
robinson <robinpaulista85@gmail.com>
2023-07-10 15:27:24 -0300

    Movendo as functions based views para o arquivo views.py dos novos apps no Django.

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from blog import views as blog_views
 from django.contrib import admin
 from django.urls import path
+from home import views as home_views
+
 
 urlpatterns = [
+    path('', home_views.home),
+    path('blog/', blog_views.blog),
     path('admin/', admin.site.urls),
 ]
