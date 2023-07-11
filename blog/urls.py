9551c05890d6642fe47ad526f7e0eeadca1bc700
robinson <robinpaulista85@gmail.com>
2023-07-11 20:01:58 -0300

    Trabalhando com URLS dinãmicas em urls.py e nos templates HTML do Django.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from blog import views
 
-from . import views
-
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.blog),
+    path('', views.blog, name='home'),
+    path('exemplo/', views.exemplo, name='exemplo'),
 ]
