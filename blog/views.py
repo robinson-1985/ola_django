14d702294ae9c8c6937342ace8593a741e2d075b
robinson <robinpaulista85@gmail.com>
2023-07-12 11:35:48 -0300

    Criando os dados de posts (data.py) e usando o loop for no template Django.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from blog.data import posts
 
 def blog(request):
     print('blog')
     
     context = {
         'text': 'Olá blog',
+        'posts': posts,
     }
     
     return render(
